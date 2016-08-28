181391ad83aeb0e7b1038ab50e198fe976bc61d5
Andras Szell <coldcue@gmail.com>
2016-08-28 17:39:40 +0200

    first commit

---
+++ src/main/java/com/poketerkep/PoketerkepMasterApplication.java
@@ -0,0 +1,12 @@
+package com.poketerkep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketerkepMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketerkepMasterApplication.class, args);
+	}
+}
